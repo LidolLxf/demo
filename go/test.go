bc372c88af761aab2caa2766fb45dd5ae547dd9e
LidolLxf <13660354959@163.com>
2024-06-04 16:18:40 +0800

    添加go.mod

--- go/test.go
+++ go/test.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 
 	"github.com/emicklei/go-restful"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 func PrintRestful() {
 	restfulParam := restful.Parameter{}
 	fmt.Println(restfulParam)
+}
+
+func PrintK8s() {
+	err := prometheus.AlreadyRegisteredError{}
+	fmt.Println(err)
 }
